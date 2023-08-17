ee1c4770dd50718c9d78944261935efc58fb90ac
Albert Colubi <albertmc89@gmail.com>
2023-08-17 08:08:12 +0200

    Test deleteToDo reducer

--- src/store/toDos/toDosReducer.test.ts
+++ src/store/toDos/toDosReducer.test.ts
@@ -1,10 +1,10 @@
 import { ToDo } from "../../types";
-import { loadToDosActionCreator, toDosReducer } from "./toDosSlice";
+import { deleteToDo, loadToDosActionCreator, toDosReducer } from "./toDosSlice";
 import { ToDoState } from "./types";
 
 describe("Given a toDosReducer reducer", () => {
-  describe("When it receives a load toDos action with two tasks", () => {
-    test("Then it should return a new state with the received two tasks", () => {
+  describe("When it receives a load toDos action with two ToDos", () => {
+    test("Then it should return a new state with the received two ToDos", () => {
       const currentToDosState: ToDoState = {
         toDos: [],
       };
@@ -26,6 +26,21 @@ describe("Given a toDosReducer reducer", () => {
       const newToDoState = toDosReducer(currentToDosState, loadToDosAction);
 
       expect(newToDoState).toHaveProperty("toDos", toDos);
+    });
+  });
+
+  describe("When it receives a state with two ToDos and delete ToDo action with the id 2", () => {
+    test("Then it should return a new state without the ToDo with id 2", () => {
+      const currentToDosState: ToDoState = {
+        toDos: [],
+      };
+
+      const toDoToDeleteId = 2;
+      const deleteToDoAction = deleteToDo(toDoToDeleteId);
+
+      const newToDoState = toDosReducer(currentToDosState, deleteToDoAction);
+
+      expect(newToDoState).not.toContain;
     });
   });
 });
