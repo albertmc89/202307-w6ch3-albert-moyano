057fcdb160be3c962d23f0172531812894b904f7
Albert Colubi <albertmc89@gmail.com>
2023-08-17 20:18:40 +0200

    Create toDosData

---
+++ src/data.ts
@@ -0,0 +1,22 @@
+export const toDosData = [
+  {
+    id: 1,
+    name: "sport",
+    isDone: true,
+  },
+  {
+    id: 2,
+    name: "study",
+    isDone: false,
+  },
+  {
+    id: 3,
+    name: "work",
+    isDone: true,
+  },
+  {
+    id: 4,
+    name: "cook",
+    isDone: false,
+  },
+];
